529892cf7c2c749aa38f25a2abdc581ce5f19b1c
ZHAO Jing <zj@ahstu.cc>
2017-02-20 13:02:26 +0800

    上传文件至 ''

---
+++ P1280.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P1280 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int n = cin.nextInt();
+		if (n % 2 == 0)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+		cin.close();
+	}
+}
---
+++ P1313.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P1313 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int N = cin.nextInt();
+		for (int i = 7; i <= N; i++)
+			if (i % 7 == 0 || Integer.toString(i).contains("7"))
+				System.out.println(i);
+		cin.close();
+	}
+}
