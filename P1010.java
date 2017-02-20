50847a295a28fd37ece92f8b8e329bf09c87ecab
ZHAO Jing <zj@ahstu.cc>
2017-02-20 13:07:10 +0800

    aoj@eclipse

---
+++ P1010.java
@@ -0,0 +1,23 @@
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class P1010 {
+	static Scanner cin = new Scanner(System.in);
+	static PrintWriter cout = new PrintWriter(System.out);
+	static {
+	}
+
+	public static void main(String[] args) {
+		int n = cin.nextInt();
+		int a = n % 1000 / 100;
+		int b = n % 100 / 10;
+		int c = n % 10;
+		if (n == a * a * a + b * b * b + c * c * c)
+			cout.println(1);
+		else
+			cout.println(0);
+		cin.close();
+		cout.close();
+	}
+}
