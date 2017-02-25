1ae36e2ffa4401c316de0488e6632da21dc3ce97
zj <zj@ahstu.cc>
2017-02-25 20:45:07 +0800

    aoj@java

---
+++ P1283.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P1283 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int[] a = {cin.nextInt(), cin.nextInt(), cin.nextInt()};
+        Arrays.sort(a);
+        if (a[0] + a[1] <= a[2])
+            System.out.println("not a triangle");
+        else if (a[0] * a[0] + a[1] * a[1] == a[2] * a[2])
+            System.out.println("yes");
+        else
+            System.out.println("no");
+        cin.close();
+    }
+}
---
+++ P1322.java
@@ -0,0 +1,5 @@
+public class P1322 {
+    public static void main(String[] args) {
+        System.out.println("I am an \"ACMer\", I love \"ACM\".");
+    }
+}
