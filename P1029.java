5ba8738b092e4e08cb61839c07f0b7c49bb9df55
zj <33470027@qq.com>
2017-02-26 15:06:48 +0800

    aoj

---
+++ P1029.java
@@ -0,0 +1,22 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class P1029 {
+    static Scanner cin = new Scanner(System.in);
+    static PrintWriter cout = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+
+        int n = cin.nextInt();
+        int[][] a = new int[n][n];
+        int tot = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                a[i][j] = cin.nextInt();
+                if (i == j || i + j == n - 1) tot += a[i][j];
+            }
+        cout.println(tot);
+        cout.close();
+        cin.close();
+    }
+}
