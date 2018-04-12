d657dc17be5e00b99f7788784db13728416ea198
zj <zj@webturing.com>
2018-04-12 10:15:50 +0800

    白色帽子f(n)=n

---
+++ aoj/P2499.java
@@ -0,0 +1,15 @@
+package aoj;
+
+import java.util.Scanner;
+
+public class P2499 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int m = cin.nextInt();
+        while (m-- > 0) {
+            System.out.println(cin.nextInt());
+        }
+        cin.close();
+    }
+
+}
