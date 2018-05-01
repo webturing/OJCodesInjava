c8be569f3873f4bf54e74597c3b1eda7428dfb1e
zj <zj@webturing.com>
2018-05-01 22:22:53 +0800

    NYIST001ADD；

---
+++ nyist/part0basic/NYIST001ADD.java
@@ -0,0 +1,13 @@
+package nyist.part0basic;
+
+import java.util.Scanner;
+
+public class NYIST001ADD {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        System.out.println(a + b);
+        cin.close();
+    }
+}
