adfe0cc1576eda07819a6c664a0689c85edf9583
zj <zj@webturing.com>
2018-05-02 15:08:17 +0800

    解密QQ号:链表操作，

---
+++ books/ahaAlgorithm/books/P1582DecodeQQNumber.java
@@ -0,0 +1,29 @@
+package books.ahaAlgorithm.books;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class P1582DecodeQQNumber {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < n; i++)
+            list.add(cin.nextInt());
+
+        boolean first = true;
+        while (!list.isEmpty()) {
+            if (first) {
+                first = false;
+            } else {
+                System.out.print(" ");
+            }
+            System.out.print(list.pollFirst());
+            if (list.isEmpty())
+                return;
+            list.addLast(list.pollFirst());
+        }
+        System.out.println();
+        cin.close();
+    }
+}
