84bea03f8c07db2bd5af7c76c18722f058b7d834
zj <zj@webturing.com>
2018-05-02 14:56:04 +0800

    小哈买书（排序去重，TreeSetM水过)

---
+++ books/ahaAlgorithm/books/P1581AhaBuyBook.java
@@ -0,0 +1,27 @@
+package books.ahaAlgorithm.books;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class P1581AhaBuyBook {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        TreeSet<Integer> sets = new TreeSet<Integer>();
+        for (int i = 0; i < n; i++)
+            sets.add(cin.nextInt());
+        System.out.println(sets.size());
+        boolean first = true;
+        for (Integer ele : sets) {
+            if (first) {
+                first = false;
+            } else {
+                System.out.print(" ");
+            }
+            System.out.print(ele);
+        }
+        System.out.println();
+
+        cin.close();
+    }
+}
