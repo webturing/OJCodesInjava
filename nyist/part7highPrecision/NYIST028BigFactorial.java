5e42cfa51201f017b3a125c08cfb7fa727e41a45
zj <zj@webturing.com>
2018-05-03 09:21:50 +0800

    大数阶乘BigInteger水过

---
+++ nyist/part7highPrecision/NYIST028BigFactorial.java
@@ -0,0 +1,19 @@
+package nyist.part7highPrecision;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 大数阶乘 Java BigInteger水过
+ */
+public class NYIST028BigFactorial {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        BigInteger prd = BigInteger.ONE;
+        for (int i = 2; i <= m; i++)
+            prd = prd.multiply(new BigInteger(Integer.toString(i)));
+        System.out.print(prd);
+        in.close();
+    }
+}
