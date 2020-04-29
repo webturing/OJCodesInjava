241967b156c4370f3f2add6fa8909a338241e63c
webturing <zj@webturing.com>
2020-04-29 21:38:09 +0800

    PAT乙级 1009说反话

---
+++ pat/basic/P1009.java
@@ -0,0 +1,20 @@
+package pat.basic;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class P1009 {
+    static Scanner cin = new Scanner(System.in);
+    static PrintWriter cout = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        String[] words = cin.nextLine().split("\\s+");
+        StringBuffer buffer = new StringBuffer();
+        for (int i = words.length - 1; i >= 0; i--) {
+            buffer.append(" " + words[i]);
+        }
+        cout.println(buffer.substring(1));
+        cin.close();
+        cout.close();
+    }
+}
