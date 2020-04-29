9f9a5ff50ec36d4a345ae98a48f46fbd2b1f21cb
webturing <zj@webturing.com>
2020-04-29 19:28:34 +0800

    3n+1环，图算法，有点难度

--- pat/basic/P1005b.java
+++ pat/basic/P1005b.java
@@ -3,13 +3,13 @@ package pat.basic;
 import java.io.PrintWriter;
 import java.util.*;
 
-public class P1005 {
+public class P1005b {
 
     static Scanner cin = new Scanner(System.in);
     static PrintWriter cout = new PrintWriter(System.out);
 
     public static void main(String[] args) {
-        TreeSet<Integer> V = new TreeSet<Integer>(Collections.<Integer>reverseOrder());
+        TreeSet<Integer> V = new TreeSet<Integer>(Collections.reverseOrder());
         int n = cin.nextInt();
         for (int i = 0; i < n; i++) V.add(cin.nextInt());
         TreeMap<Integer, Set<Integer>> G = new TreeMap<Integer, Set<Integer>>();
@@ -25,12 +25,14 @@ public class P1005 {
                 if (V.contains(n)) G.get(v).add(n);
             }
         }
+        cout.println(G);
         TreeMap<Integer, Integer> D = new TreeMap<Integer, Integer>();
         for (Integer v : V) {
             for (Integer t : G.get(v)) {
                 D.put(t, 1);
             }
         }
+        cout.println(D);
         int j = 0;
         for (Integer v : V) {
             if (!D.containsKey(v)) {
